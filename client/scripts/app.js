f34aeaa2ad36b8a277882124d1f7f1a1f13aab7f
Bill Ramsey <billramsey@gmail.com>
2016-05-02 14:37:55 -0700

    Pass some tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,54 @@
 // YOUR CODE HERE:
 
+
+
+  $( document ).ready(function() {
+    $( "#submitText" ).click(function() {
+      alert( 'Handler for .click() called.' );
+
+
+
+
+    });
+  
+
+  });
+
+/*
+  var result = _.escape('string');
+  console.log(result); 
+
+        url: "https://api.parse.com/1/classes/messages",
+*/
+  var app = {
+    init: function() {
+      return true;
+    },
+    send: function(message) {
+      // $.post( "https://api.parse.com/1/classes/messages", function( data ) {
+      //   console.log("posted");
+      // });
+
+
+      $.ajax({
+        type: "POST",
+        url: "https://api.parse.com/1/classes/messages",
+        data: JSON.stringify(message),
+        success: function() {
+        },
+        dataType: 'json'
+      });
+
+    },
+    fetch: function() {
+      $.ajax({
+        type: "GET",
+        success: function() {
+        }
+      });
+    },
+    clearMessages() {
+      $('#chats').html('');
+    }
+  
+  };
