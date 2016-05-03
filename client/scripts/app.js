95ad2af867d35f551abf98944e9745f75885c3c7
shadowfool <aaronf101@gmail.com>
2016-05-02 20:29:19 -0700

    Clean up code, refactor sanitize, move chats below main

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,55 +1,41 @@
-// YOUR CODE HERE:
 
-///hacking ideas:
-// put stuff in the user name
-//request username hack
-
-
-//TODO
-//bold friends
-//create rooms
-//change name
+//change user name
+//Optional, better handle loading new entries instead of wipe/clear
 
   var app = {
-    server:"https://api.parse.com/1/classes/messages",
-    currentRoom : '',
+    server: 'https://api.parse.com/1/classes/messages',
+    currentRoom: '',
     friends: [],
+    rooms: [],
     init: function(name) {
-      console.log('init called' + Array.prototype.slice.call(arguments));
-      console.log(name);
     },
     send: function(message) {
       $.ajax({
-        type:'POST',
+        type: 'POST',
         url: app.server,
         data: JSON.stringify(message),
         success: function() {
-          console.log('success');
         },
         error: function(errMessage) {
-          console.log('error');
         },
         dataType: 'json'
       });
-
     },
     fetch: function() {
       $.ajax({
-        type: "GET",
+        type: 'GET',
         url: app.server,
         success: function(data) {
           app.clearMessages();
           _.forEach(data.results, function(message) {
-            var room = _.escape(message.roomname).trim();
+            var room = app.sanitize(message.roomname).trim();
             if (_.indexOf(app.rooms, room) === -1 && room !== '') {
               app.rooms.push(room);
               app.addRoom(room);
             }
-
             if (room === app.currentRoom) {
               app.addMessage(message);
             }
-             
           });
         }
       });
@@ -57,27 +43,24 @@
     clearMessages: function() {
       $('#chats').empty();
     },
-    rooms: ['lobby'],
     addMessage: function(message) {
-      var userName = _.escape(message.username);
+      var userName = app.sanitize(message.username);
       var clazz = '';
       if (app.friends.indexOf(userName) >= 0) {
-        clazz = "friend";
+        clazz = ' friend';
       }
-      $('<div class="message" ><a href="" class="username ' + clazz + '">' + userName  + '</a> ' + _.escape(message.text) + '</div>').appendTo('#chats');
-      
-      // $('<div class="username" >' + _.escape(message.username) + '</div>').appendTo('#main');
+      $('<div class="chat" ><a href="" class="chat username' + clazz + '">' + userName + '</a> ' + app.sanitize(message.text) + '</div>').appendTo('#chats');
     },
     addRoom: function(room) {
       $('<option value="' + room + '">' + room + '</option>').appendTo('#roomSelectData');
     },
     addRemoveFriend: function(val) {
-      var friend = _.escape(val);
+      var friend = app.sanitize(val);
       var friendIndex = _.indexOf(app.friends, friend);
       if (friendIndex === -1) {
         app.friends.push(friend);
       } else {
-        app.friends.splice(friendIndex,1);
+        app.friends.splice(friendIndex, 1);
         //remove friend
       }
     },
@@ -91,32 +74,34 @@
     },
     filterRooms: function(room) {
       app.currentRoom = room;
+    },
+    sanitize: function(data) {
+      return _.escape(data);
     }
   };
 
-  app.fetch();
-
 
   $( document ).ready(function() {
-    $( "#send" ).on("submit", function(event) {
+    setInterval(app.fetch, 1000);
+    //Send Message
+    $( '#send' ).on('submit', function(event) {
       app.handleSubmit($('#message').val());
       event.preventDefault();
       $('#message').val('');
-
     });
 
-    $( "#main").on("click", ".username", function(event) {
+    //Friend/Defriend
+    $( '#chats').on('click', '.username', function(event) {
       event.preventDefault();
       app.addRemoveFriend($(this).text());
     });
 
-    $( "#roomSelect").on("input", function(event) {
+    //Room Select
+    $( '#roomSelect').on('input', function(event) {
       currentRoom = $(this).val();
       app.filterRooms($(this).val());
     });
 
-
   });
-  setInterval(app.fetch, 1000);
 
 
