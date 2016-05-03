dccc9e954ba77eeb508243ca229b71c1f96c41f6
shadowfool <aaronf101@gmail.com>
2016-05-02 20:00:27 -0700

    Implement rooms and friends

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -12,6 +12,8 @@
 
   var app = {
     server:"https://api.parse.com/1/classes/messages",
+    currentRoom : '',
+    friends: [],
     init: function(name) {
       console.log('init called' + Array.prototype.slice.call(arguments));
       console.log(name);
@@ -39,11 +41,15 @@
           app.clearMessages();
           _.forEach(data.results, function(message) {
             var room = _.escape(message.roomname).trim();
-            if (_.indexOf(app.rooms, room) === -1 && room !== ''){
+            if (_.indexOf(app.rooms, room) === -1 && room !== '') {
               app.rooms.push(room);
               app.addRoom(room);
             }
-            app.addMessage(message);
+
+            if (room === app.currentRoom) {
+              app.addMessage(message);
+            }
+             
           });
         }
       });
@@ -53,22 +59,38 @@
     },
     rooms: ['lobby'],
     addMessage: function(message) {
-      $('<div class="message" ><a href="" class="username">' + _.escape(message.username) + '</a> ' + _.escape(message.text) + '</div>').appendTo('#chats');
+      var userName = _.escape(message.username);
+      var clazz = '';
+      if (app.friends.indexOf(userName) >= 0) {
+        clazz = "friend";
+      }
+      $('<div class="message" ><a href="" class="username ' + clazz + '">' + userName  + '</a> ' + _.escape(message.text) + '</div>').appendTo('#chats');
+      
       // $('<div class="username" >' + _.escape(message.username) + '</div>').appendTo('#main');
     },
     addRoom: function(room) {
-      $('<option value="' + room + '">' + room + '</option>').appendTo('#roomSelect');
+      $('<option value="' + room + '">' + room + '</option>').appendTo('#roomSelectData');
     },
-    addFriend: function(val) {
+    addRemoveFriend: function(val) {
       var friend = _.escape(val);
+      var friendIndex = _.indexOf(app.friends, friend);
+      if (friendIndex === -1) {
+        app.friends.push(friend);
+      } else {
+        app.friends.splice(friendIndex,1);
+        //remove friend
+      }
     },
     handleSubmit: function(val) {
       var username = window.location.search.slice(10);
       app.send({
         username: username,
         text: val,
-        roomname: 'lobby'
+        roomname: currentRoom
       });
+    },
+    filterRooms: function(room) {
+      app.currentRoom = room;
     }
   };
 
@@ -85,8 +107,15 @@
 
     $( "#main").on("click", ".username", function(event) {
       event.preventDefault();
-      app.addFriend($(this).text());
+      app.addRemoveFriend($(this).text());
     });
+
+    $( "#roomSelect").on("input", function(event) {
+      currentRoom = $(this).val();
+      app.filterRooms($(this).val());
+    });
+
+
   });
   setInterval(app.fetch, 1000);
 
