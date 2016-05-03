a2433153c257d1538c8a52d577d4c75452c33a63
shadowfool <aaronf101@gmail.com>
2016-05-02 21:40:23 -0700

    Implement lazy load refinement

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,14 +7,21 @@
     currentRoom: '',
     friends: [],
     rooms: [],
+    chatLog: {},
+    username: window.location.search.slice(10),
     init: function(name) {
     },
     send: function(message) {
+      // $('.spinner').css('visibility', 'visible');
+      $('.spinner').show();
       $.ajax({
         type: 'POST',
         url: app.server,
         data: JSON.stringify(message),
         success: function() {
+          setTimeout( function() {
+            $('.spinner').hide();
+          }, 500);
         },
         error: function(errMessage) {
         },
@@ -26,15 +33,19 @@
         type: 'GET',
         url: app.server,
         success: function(data) {
-          app.clearMessages();
-          _.forEach(data.results, function(message) {
-            var room = app.sanitize(message.roomname).trim();
-            if (_.indexOf(app.rooms, room) === -1 && room !== '') {
-              app.rooms.push(room);
-              app.addRoom(room);
-            }
-            if (room === app.currentRoom) {
-              app.addMessage(message);
+          //reversed to show up in reverse chron order
+          _.forEach(data.results.reverse(), function(obj) {
+            //console.log(obj, app.chatLog[obj.objectId]);
+            if (app.chatLog[obj.objectId] === undefined) {
+              app.chatLog[obj.objectId] = obj;
+
+              var room = app.sanitize(obj.roomname).trim();
+              //console.log(room);
+              if (_.indexOf(app.rooms, room) === -1 && room !== '') {
+                app.rooms.push(room);
+                app.addRoom(room);
+              }
+              app.addMessage(obj);
             }
           });
         }
@@ -49,7 +60,7 @@
       if (app.friends.indexOf(userName) >= 0) {
         clazz = ' friend';
       }
-      $('<div class="chat" ><a href="" class="chat username' + clazz + '">' + userName + '</a> ' + app.sanitize(message.text) + '</div>').appendTo('#chats');
+      $('<div class="chat" ><a href="" class="chat username' + clazz + '">' + userName + '</a> ' + app.sanitize(message.text) + '</div>').prependTo('#chats');
     },
     addRoom: function(room) {
       $('<option value="' + room + '">' + room + '</option>').appendTo('#roomSelectData');
@@ -61,19 +72,25 @@
         app.friends.push(friend);
       } else {
         app.friends.splice(friendIndex, 1);
-        //remove friend
       }
     },
     handleSubmit: function(val) {
-      var username = window.location.search.slice(10);
       app.send({
-        username: username,
+        username: app.username,
         text: val,
-        roomname: currentRoom
+        roomname: app.currentRoom
       });
     },
     filterRooms: function(room) {
       app.currentRoom = room;
+      //clear messages with wrong room
+      app.clearMessages();
+      // display messages with the same room as current room
+      _.forEach(app.chatLog, function(chat) {
+        if (chat.roomname === currentRoom) {
+          app.addMessage(chat);
+        }
+      });
     },
     sanitize: function(data) {
       return _.escape(data);
